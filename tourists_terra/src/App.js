14711e315fccec0d0daa6aa6d378255aafd40908
Asad Rehman <112416011+TrendoCoder@users.noreply.github.com>
2023-04-13 12:48:59 +0500

    start

---
+++ tourists_terra/src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import NavBar from "./Components/NavBar/navBar";
+function App() {
+  return (
+    <div>
+      <NavBar />
+    </div>
+  );
+}
+
+export default App;
---
+++ tourists_terra/src/Components/NavBar/navBar.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import "./navBar.css";
+function navBar() {
+  return (
+    <div>
+      <div className="big-Container">
+        <div className="small-Container">
+          <div>
+            <img src="" alt="logo" />
+            <h3>TouristsTerra</h3>
+          </div>
+          <div>
+            <input type="text" />
+          </div>
+          <div>
+            
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default navBar;
