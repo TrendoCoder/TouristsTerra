73ac7f6951150b53959e02a169a3528645e3d3b4
Asad Rehman <112416011+TrendoCoder@users.noreply.github.com>
2023-04-13 13:20:35 +0500

    navbar

--- tourists_terra/src/Components/NavBar/navBar.jsx
+++ tourists_terra/src/Components/NavBar/navBar.jsx
@@ -3,17 +3,20 @@ import "./navBar.css";
 function navBar() {
   return (
     <div>
-      <div className="big-Container">
-        <div className="small-Container">
-          <div>
+      <div id="big-Container">
+        <div id="small-Container">
+          <div id="logo-section">
             <img src="" alt="logo" />
             <h3>TouristsTerra</h3>
           </div>
-          <div>
+          <div id="search-section">
             <input type="text" />
           </div>
-          <div>
-            
+          <div id="small-menu">
+          <i class="fa-solid fa-house"></i>
+          <i class="fa-sharp fa-solid fa-message-middle"></i>
+          <i class="fa-solid fa-bell"></i>
+          <i class="fa-solid fa-user-tie"></i>
           </div>
         </div>
       </div>
