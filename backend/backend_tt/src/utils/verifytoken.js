c70dfee5a052b7142b2869ac20ce52478f982ae3
Asad Rehman <112416011+TrendoCoder@users.noreply.github.com>
2023-10-18 00:58:39 +0500

    Update verifytoken.js

--- backend/backend_tt/src/utils/verifytoken.js
+++ backend/backend_tt/src/utils/verifytoken.js
@@ -1,26 +1,25 @@
 const jwt = require("jsonwebtoken");
 const { createError } = require("./error");
 
-const verifyToken = (req,res,next) => {
-    const token = req.cookies.access_token;
-    if(!token){
-        return next(createError(401,"You are not authenticated"))
-    }
-    jwt.verify(token,"jwtPrivateKey",(err,user)=>{
-        if(err) return next(createError(403,"Token is not valid!"));
-        req.user = user;
-        next(); 
-    })
-}
+const verifyToken = (req, res, next) => {
+  const token = req.cookies.access_token;
+  if (!token) {
+    return next(createError(401, "You are not authenticated"));
+  }
+  jwt.verify(token, "jwtPrivateKey", (err, user) => {
+    if (err) return next(createError(403, "Token is not valid!"));
+    req.user = user;
+    next();
+  });
+};
 module.exports.verifyToken = verifyToken;
 
-const verifyUser = (req,res,next)=>{
-    verifyToken(req,res,next, ()=>{
-        if(req.user.id === req.params.id || req.user.isAdmin)
-        {
-            next();
-        }else{
-            if(err) return next(createError(403,"You are not "));
-        }
-    })
-}
+const verifyUser = (req, res, next) => {
+  verifyToken(req, res, next, () => {
+    if (req.user.id === req.params.id || req.user.isAdmin) {
+      next();
+    } else {
+      if (err) return next(createError(403, "You are not "));
+    }
+  });
+};
