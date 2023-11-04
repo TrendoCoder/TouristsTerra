b8f0bddb95aa859609a52b3acd6e893560c0c242
Shamir Hussain <112416011+TrendoCoder@users.noreply.github.com>
2023-11-04 19:25:11 +0500

    Update room.js

--- backend/backend_tt/src/models/hotelsmodel/room.js
+++ backend/backend_tt/src/models/hotelsmodel/room.js
@@ -1,34 +1,37 @@
 var mongoose = require("mongoose");
 
-const {Schema} = mongoose;
+const { Schema } = mongoose;
 
-const RoomSchema = new mongoose.Schema({
-    title:{
-        type: String,
-        required:true
+const RoomSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
     },
-    price:{
-        type: Number,
-        required:true
+    price: {
+      type: Number,
+      required: true,
     },
-    newPeople:{
-        type: Number,
-        required:true
+    newPeople: {
+      type: Number,
+      required: true,
     },
-    roomDescription:{
-        type: String,
-        required:true
+    roomDescription: {
+      type: String,
+      required: true,
     },
-    roomNumber:[{
-        number:Number,
-        unavailableDates: {type:[Date]}
-    }]
-},
-{
-    timestamps:true
-}
+    roomNumber: [
+      {
+        number: Number,
+        unavailableDates: { type: [Date] },
+      },
+    ],
+  },
+  {
+    timestamps: true,
+  }
 );
 
-var Room =  mongoose.model("Room", RoomSchema);
+var Room = mongoose.model("Room", RoomSchema);
 
 module.exports.Room = Room;
