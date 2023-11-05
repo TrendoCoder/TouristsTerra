832526b84ac43b1ec7cbbcd3111fe9c4ba4895be
RanaUmairNazar <128068171+RanaUmairNazar@users.noreply.github.com>
2023-11-06 04:07:19 +0500

    Shop front end

---
+++ tourists_terra/src/Components/User_Module/shoppage/shophomepage/ProductDetail.jsx
@@ -0,0 +1,88 @@
+import React, { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import { Link } from 'react-router-dom'
+import Navbar from '../../homepage/navbar/navBar';
+import AccommodationAdSection from '../../accommodationpage/accomoadsection/accomoadsection';
+import Footer from '../../accommodationpage/footer/footer'
+import MenuBar from '../../homepage/menubar/menuBar'
+
+const ProductDetail = () => {
+    const [product, setProduct] = useState(null);
+    const { productId } = useParams();
+
+    useEffect(() => {
+        fetch(`http://localhost:3001/api/product/${productId}`)
+            .then((response) => response.json())
+            .then((data) => setProduct(data))
+            .catch((error) => console.error('Error fetching product:', error));
+    }, [productId]);
+
+    if (!product) {
+        return <div className="text-center">Loading...</div>;
+    }
+
+    return (
+        <div>
+            <Navbar />
+            <div id='accomo-ad-container'>
+                    <AccommodationAdSection />
+                    <div id='opacity-ad'>
+                        <Link to=""><h1>Wana Shop?</h1></Link>
+                    </div>
+                </div>
+
+
+            <div id='menu-acc'>
+                <MenuBar />
+            </div>
+            <br />
+            <br />
+
+            <div className="container mx-auto p-4">
+                <div className="bg-white shadow overflow-hidden sm:rounded-lg">
+                    <img className="w-full h-64 object-cover" src={product.image} alt={product.name} />
+                    <div className="px-4 py-5 sm:px-6">
+                        <h3 className="text-lg leading-6 font-medium text-gray-900">{product.name}</h3>
+                        <p className="mt-1 max-w-2xl text-sm text-gray-500">Category: {product.category?.name}</p>
+                    </div>
+                    <div className="border-t border-gray-200">
+                        <dl>
+                            <div className="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                                <dt className="text-sm font-medium text-gray-500">Product Details</dt>
+                                <dd className="mt-1 text-sm text-gray-900 sm:col-span-2">{product.description}</dd>
+                            </div>
+                            <div className="bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                                <dt className="text-sm font-medium text-gray-500">Price</dt>
+                                <dd className="mt-1 text-sm text-gray-900 sm:col-span-2">${product.price.toFixed(2)}</dd>
+                            </div>
+                            {/* Add more product details here as needed */}
+                            <div className="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                                <dt className="text-sm font-medium text-gray-500">Quantity Available</dt>
+                                <dd className="mt-1 text-sm text-gray-900 sm:col-span-2">{product.quantity}</dd>
+                            </div>
+                            <div className="bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                                <dt className="text-sm font-medium text-gray-500">Rating</dt>
+                                <dd className="mt-1 text-sm text-gray-900 sm:col-span-2">
+                                    {product.ratings} <span className="text-yellow-400">★</span>
+                                </dd>
+                            </div>
+                            <div className="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                                <dt className="text-sm font-medium text-gray-500">Seller Information</dt>
+                                <dd className="mt-1 text-sm text-gray-900 sm:col-span-2">{product.seller.userName} - {product.seller.email}</dd>
+                            </div>
+                            <div className="bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                                <dt className="text-sm font-medium text-gray-500">Stock Availability</dt>
+                                <dd className="mt-1 text-sm text-gray-900 sm:col-span-2">
+                                    {product.inStock ? 'In Stock' : 'Out of Stock'}
+                                </dd>
+                            </div>
+                        </dl>
+                    </div>
+                </div>
+            </div>
+            <Footer />
+        </div>
+    );
+};
+
+export default ProductDetail;
--- tourists_terra/src/Components/User_Module/shoppage/shophomepage/shophomepage.jsx
+++ tourists_terra/src/Components/User_Module/shoppage/shophomepage/shophomepage.jsx
@@ -1,7 +1,94 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { Link } from 'react-router-dom'
 import "./shophomepage.css";
+import Navbar from '../../homepage/navbar/navBar'
+import AccommodationAdSection from '../../accommodationpage/accomoadsection/accomoadsection'
+import Footer from '../../accommodationpage/footer/footer'
+import MenuBar from '../../homepage/menubar/menuBar'
+
+// Helper function to group products by category name
+const groupByCategory = (products) => {
+  return products.reduce((acc, product) => {
+    const key = product.category?.name || 'Other';
+    if (!acc[key]) {
+      acc[key] = [];
+    }
+    acc[key].push(product);
+    return acc;
+  }, {});
+};
+
 const ShopHomePage = () => {
-  return <div>this is the home page of shop module</div>;
+
+  const [products, setProducts] = useState([]);
+  const [groupedProducts, setGroupedProducts] = useState({});
+
+  useEffect(() => {
+
+    fetch('http://localhost:3001/api/product')
+      .then((response) => response.json())
+      .then((data) => {
+        setProducts(data);
+        setGroupedProducts(groupByCategory(data));
+      })
+      .catch((error) => {
+        console.error('Error fetching products:', error);
+      });
+  }, []);
+
+
+  return (
+    <>
+      <div>
+        <Navbar />
+        <div id='accomo-ad-container'>
+          <AccommodationAdSection />
+          <div id='opacity-ad'>
+            <Link to=""><h1>Wana Shop?</h1></Link>
+          </div>
+        </div>
+
+        <div id='menu-acc'>
+          <MenuBar />
+        </div>
+        <br />
+        <br />
+
+        <div className="p-4">
+          {Object.entries(groupedProducts).map(([categoryName, products]) => (
+            <div key={categoryName} className="mb-8">
+              <h2 className="text-2xl font-bold my-6">{categoryName}</h2>
+              <div className="flex flex-wrap -mx-4">
+                {products.map((product) => (
+                  <Link to={`/product/${product._id}`} key={product._id} className="p-4 w-full sm:w-1/2 lg:w-1/4">
+                    <div className="max-w-sm rounded overflow-hidden shadow-lg bg-white">
+                      <img className="w-full h-48 object-cover" src={product.image} alt={product.name} />
+                      <div className="px-6 py-4">
+                        <div className="font-bold text-xl mb-2">{product.name}</div>
+                        <p className="text-gray-700 text-base">Price: ${product.price.toFixed(2)}</p>
+                        <p className="text-gray-700 text-base">Description: {product.description}</p>
+                        <p className="text-gray-700 text-base">Quantity: {product.quantity}</p>
+                        <p className="text-gray-700 text-base flex items-center">
+                          Rating: <span className="ml-2 text-yellow-400">{product.ratings} <i className="fas fa-star"></i></span>
+                        </p>
+                        {/* <p className="text-gray-700 text-base">Seller ID: {product.seller}</p> */}
+                        <p className={`text-gray-700 text-base ${product.inStock ? 'text-green-500' : 'text-red-500'}`}>
+                          In Stock: {product.inStock ? 'Yes' : 'No'}
+                        </p>
+                      </div>
+                    </div>
+                  </Link>
+                ))}
+              </div>
+            </div>
+          ))}
+        </div>
+
+        <Footer />
+
+      </div>
+    </>
+  )
 };
 
 export default ShopHomePage;
