e099ffa31ce9d53a1d9c7f5e6d639a0f7c64f691
MuhammadAjeel <muhammadajeel1999@gmail.com>
2023-11-09 15:58:39 +0500

    working on filters

--- tourists_terra/src/Components/User_Module/shoppage/shophomepage/shophomepage.jsx
+++ tourists_terra/src/Components/User_Module/shoppage/shophomepage/shophomepage.jsx
@@ -22,20 +22,136 @@ const ShopHomePage = () => {
   const [products, setProducts] = useState([]);
   const [groupedProducts, setGroupedProducts] = useState({});
 
+  
+
+
+
   useEffect(() => {
-    fetch("http://localhost:3001/api/product")
-      .then((response) => response.json())
-      .then((data) => {
-        setProducts(data);
-        setGroupedProducts(groupByCategory(data));
-      })
-      .catch((error) => {
-        console.error("Error fetching products:", error);
-      });
+   fetchProducts();
   }, []);
+  const fetchProducts = ()=>{
+    fetch("http://localhost:3001/api/product")
+    .then((response) => response.json())
+    .then((data) => {
+      setProducts(data);
+      setGroupedProducts(groupByCategory(data));
+    })
+    .catch((error) => {
+      console.error("Error fetching products:", error);
+    });
+  }
+
+  const [filter, setFilter] = useState({
+    quantity: [],
+    price: { from: 0, to: 10000 },
+    rating: [],
+  });
+  useEffect(() => {
+    console.log("Filter updated:", filter);
+    let filteredProducts = [...products]; 
+    if (filter.rating.includes("1 Star")) { 
+      filteredProducts = filteredProducts.filter(product => product.ratings <= 1);
+    }
+    if (filter.rating.includes("2 Star")) { 
+      filteredProducts = filteredProducts.filter(product => product.ratings <= 2);
+    }
+     if (filter.rating.includes("3 Star")) { 
+      filteredProducts = filteredProducts.filter(product => product.ratings <= 3);
+    }
+    if (filter.rating.includes("4 Star")) { 
+      filteredProducts = filteredProducts.filter(product => product.ratings === 4);
+      console.log("Filter updated:",filteredProducts);
+    }
+    if (filter.rating.includes("5 Star")) { 
+      filteredProducts = filteredProducts.filter(product => product.ratings <= 5);
+    }
+    if(filter.rating.length<1) fetchProducts();
+
+    if (filter.quantity.includes("Small")) { 
+      filteredProducts = filteredProducts.filter(product => product.quantity <= 50);
+    }
+    if (filter.quantity.includes("Meduim")) { 
+      filteredProducts = filteredProducts.filter(product => product.quantity <= 100);
+    }
+     if (filter.quantity.includes("Large")) { 
+      filteredProducts = filteredProducts.filter(product => product.quantity > 100);
+    }
+    if(filter.quantity.length<1) fetchProducts();
+
+  
+    setGroupedProducts(groupByCategory(filteredProducts));
+  
+    setProducts(filteredProducts);
+  
+  }, [filter]); 
+
+  const handleQuantityChange = (size) => {
+    setFilter((prevFilter) => {
+      const newQuantity = prevFilter.quantity.includes(size)
+        ? prevFilter.quantity.filter((s) => s !== size) // Remove size if it's already included
+        : [...prevFilter.quantity, size]; // Add size if it's not included
+
+      // Log the change
+      console.log(`Size ${size} is now ${newQuantity.includes(size) ? 'selected' : 'unselected'}.`);
+
+      // Return the updated filter object
+      return {
+        ...prevFilter,
+        quantity: newQuantity,
+      };
+    });
+  };
+
+  const handlePriceChange = (field, value) => {
+    setFilter((prevFilter) => ({
+      ...prevFilter,
+      price: {
+        ...prevFilter.price,
+        [field]: Number(value), // Ensure the value is stored as a Number
+      },
+    }));
+  };
+  const handleRatingChange = (selectedRating) => {
+    setFilter((prevFilter) => {
+      // Toggle the selection of the rating
+      const newRatings = prevFilter.rating.includes(selectedRating)
+        ? prevFilter.rating.filter(rating => rating !== selectedRating) // Remove it if it's there
+        : [...prevFilter.rating, selectedRating]; // Add it if it's not
+
+      // Log the selected ratings
+      console.log(`Rating ${selectedRating} is now ${newRatings.includes(selectedRating) ? 'selected' : 'unselected'}.`);
+
+      return {
+        ...prevFilter,
+        rating: newRatings,
+      };
+    });
+  };
+  const handleResetPrice = () => {
+    setFilter((prevFilter) => ({
+      ...prevFilter,
+      price: {
+        from: 0,
+        to: 10000,
+      },
+    }));
+  };
+
+
+
+  
+
+
+  const resetFilters = () => {
+    setFilter({
+      quantity: new Set(),
+      price: { from: 0, to: 10000 },
+      rating: new Set(),
+    });
+  };
   if (!products) {
     return <div className="h-screen flex justify-center items-center">Loading...</div>;
-}
+  }
   return (
     <>
       <div>
@@ -121,7 +237,7 @@ const ShopHomePage = () => {
                     <div class="mt-1 space-y-2">
                       <details class="overflow-hidden rounded border border-gray-300 [&_summary::-webkit-details-marker]:hidden">
                         <summary class="flex cursor-pointer items-center justify-between gap-2 p-4 text-gray-900 transition">
-                          <span class="text-sm font-medium"> Language </span>
+                          <span class="text-sm font-medium"> Quantity </span>
 
                           <span class="transition group-open:-rotate-180">
                             <svg
@@ -156,73 +272,23 @@ const ShopHomePage = () => {
                             </button>
                           </header>
 
-                          <ul class="space-y-1 border-t border-gray-200 p-4">
-                            <li>
-                              <label
-                                for="FilterInStock"
-                                class="inline-flex items-center gap-2"
-                              >
-                                <input
-                                  type="checkbox"
-                                  id="FilterInStock"
-                                  class="h-5 w-5 rounded border-gray-300"
-                                />
-
-                                <span class="text-sm font-medium text-gray-700">
-                                  Urdu
-                                </span>
-                              </label>
-                            </li>
-
-                            <li>
-                              <label
-                                for="FilterPreOrder"
-                                class="inline-flex items-center gap-2"
-                              >
-                                <input
-                                  type="checkbox"
-                                  id="FilterPreOrder"
-                                  class="h-5 w-5 rounded border-gray-300"
-                                />
-
-                                <span class="text-sm font-medium text-gray-700">
-                                  English
-                                </span>
-                              </label>
-                            </li>
-
-                            <li>
-                              <label
-                                for="FilterOutOfStock"
-                                class="inline-flex items-center gap-2"
-                              >
-                                <input
-                                  type="checkbox"
-                                  id="FilterOutOfStock"
-                                  class="h-5 w-5 rounded border-gray-300"
-                                />
-
-                                <span class="text-sm font-medium text-gray-700">
-                                  Punjabi
-                                </span>
-                              </label>
-                            </li>
-                            <li>
-                              <label
-                                for="FilterOutOfStock"
-                                class="inline-flex items-center gap-2"
-                              >
-                                <input
-                                  type="checkbox"
-                                  id="FilterOutOfStock"
-                                  class="h-5 w-5 rounded border-gray-300"
-                                />
-
-                                <span class="text-sm font-medium text-gray-700">
-                                  Sindhi
-                                </span>
-                              </label>
-                            </li>
+                          <ul className="space-y-1 border-t border-gray-200 p-4">
+                            {['Small', 'Medium', 'Large'].map((size) => (
+                              <li key={size}>
+                                <label htmlFor={`Filter${size}`} className="inline-flex items-center gap-2">
+                                  <input
+                                    type="checkbox"
+                                    id={`Filter${size}`}
+                                    checked={filter.quantity.includes(size)}
+                                    onChange={() => handleQuantityChange(size)}
+                                    className="h-5 w-5 rounded border-gray-300"
+                                  />
+                                  <span className="text-sm font-medium text-gray-700">
+                                    {size}
+                                  </span>
+                                </label>
+                              </li>
+                            ))}
                           </ul>
                         </div>
                       </details>
@@ -258,9 +324,11 @@ const ShopHomePage = () => {
                             <button
                               type="button"
                               class="text-sm text-gray-900 underline underline-offset-4"
+                              onClick={handleResetPrice}
                             >
                               Reset
                             </button>
+
                           </header>
 
                           <div class="border-t border-gray-200 p-4">
@@ -275,7 +343,9 @@ const ShopHomePage = () => {
                                   type="number"
                                   id="FilterPriceFrom"
                                   placeholder="From"
-                                  class="w-full rounded-md border-gray-200 shadow-sm sm:text-sm"
+                                  value={filter.price.from}
+                                  onChange={(e) => handlePriceChange('from', e.target.value)}
+                                  className="w-full rounded-md border-gray-200 shadow-sm sm:text-sm"
                                 />
                               </label>
 
@@ -289,7 +359,9 @@ const ShopHomePage = () => {
                                   type="number"
                                   id="FilterPriceTo"
                                   placeholder="To"
-                                  class="w-full rounded-md border-gray-200 shadow-sm sm:text-sm"
+                                  value={filter.price.to}
+                                  onChange={(e) => handlePriceChange('to', e.target.value)}
+                                  className="w-full rounded-md border-gray-200 shadow-sm sm:text-sm"
                                 />
                               </label>
                             </div>
@@ -299,7 +371,7 @@ const ShopHomePage = () => {
 
                       <details class="overflow-hidden rounded border border-gray-300 [&_summary::-webkit-details-marker]:hidden">
                         <summary class="flex cursor-pointer items-center justify-between gap-2 p-4 text-gray-900 transition">
-                          <span class="text-sm font-medium"> City </span>
+                          <span class="text-sm font-medium"> Rating </span>
 
                           <span class="transition group-open:-rotate-180">
                             <svg
@@ -334,109 +406,26 @@ const ShopHomePage = () => {
                             </button>
                           </header>
 
-                          <ul class="space-y-1 border-t border-gray-200 p-4">
-                            <li>
-                              <label
-                                for="FilterRed"
-                                class="inline-flex items-center gap-2"
-                              >
-                                <input
-                                  type="checkbox"
-                                  id="FilterRed"
-                                  class="h-5 w-5 rounded border-gray-300"
-                                />
-
-                                <span class="text-sm font-medium text-gray-700">
-                                  Lahore
-                                </span>
-                              </label>
-                            </li>
-
-                            <li>
-                              <label
-                                for="FilterBlue"
-                                class="inline-flex items-center gap-2"
-                              >
-                                <input
-                                  type="checkbox"
-                                  id="FilterBlue"
-                                  class="h-5 w-5 rounded border-gray-300"
-                                />
-
-                                <span class="text-sm font-medium text-gray-700">
-                                  Karachi
-                                </span>
-                              </label>
-                            </li>
-
-                            <li>
-                              <label
-                                for="FilterGreen"
-                                class="inline-flex items-center gap-2"
-                              >
-                                <input
-                                  type="checkbox"
-                                  id="FilterGreen"
-                                  class="h-5 w-5 rounded border-gray-300"
-                                />
-
-                                <span class="text-sm font-medium text-gray-700">
-                                  Kashmir
-                                </span>
-                              </label>
-                            </li>
-
-                            <li>
-                              <label
-                                for="FilterOrange"
-                                class="inline-flex items-center gap-2"
-                              >
-                                <input
-                                  type="checkbox"
-                                  id="FilterOrange"
-                                  class="h-5 w-5 rounded border-gray-300"
-                                />
-
-                                <span class="text-sm font-medium text-gray-700">
-                                  Naran
-                                </span>
-                              </label>
-                            </li>
-
-                            <li>
-                              <label
-                                for="FilterPurple"
-                                class="inline-flex items-center gap-2"
-                              >
-                                <input
-                                  type="checkbox"
-                                  id="FilterPurple"
-                                  class="h-5 w-5 rounded border-gray-300"
-                                />
-
-                                <span class="text-sm font-medium text-gray-700">
-                                  Swat
-                                </span>
-                              </label>
-                            </li>
-
-                            <li>
-                              <label
-                                for="FilterTeal"
-                                class="inline-flex items-center gap-2"
-                              >
-                                <input
-                                  type="checkbox"
-                                  id="FilterTeal"
-                                  class="h-5 w-5 rounded border-gray-300"
-                                />
-
-                                <span class="text-sm font-medium text-gray-700">
-                                  Quetta
-                                </span>
-                              </label>
-                            </li>
+                          <ul className="space-y-1 border-t border-gray-200 p-4">
+                            {['1 Star', '2 Star', '3 Star', '4 Star', '5 Star'].map((rating) => (
+                              <li key={rating}>
+                                <label htmlFor={`Filter${rating.replace(' ', '')}`} className="inline-flex items-center gap-2">
+                                  <input
+                                    type="checkbox"
+                                    id={`Filter${rating.replace(' ', '')}`}
+                                    checked={filter.rating.includes(rating)}
+                                    onChange={() => handleRatingChange(rating)}
+                                    className="h-5 w-5 rounded border-gray-300"
+                                  />
+                                  <span className="text-sm font-medium text-gray-700">
+                                    {rating}
+                                  </span>
+                                </label>
+                              </li>
+                            ))}
                           </ul>
+
+
                         </div>
                       </details>
                     </div>
@@ -487,11 +476,10 @@ const ShopHomePage = () => {
                                     </span>
                                   </p>
                                   <p
-                                    className={`text-gray-700 text-base ${
-                                      product.inStock
-                                        ? "text-green-500"
-                                        : "text-red-500"
-                                    }`}
+                                    className={`text-gray-700 text-base ${product.inStock
+                                      ? "text-green-500"
+                                      : "text-red-500"
+                                      }`}
                                   >
                                     Avalible Status:{" "}
                                     {product.inStock ? "Yes" : "No"}
